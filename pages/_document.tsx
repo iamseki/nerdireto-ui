12911f333209380ac208d7b1a19f7439bf91dd98
faradayseki <chrisyuri_19@hotmail.com>
2020-05-17 22:51:44 -0300

    fix: styled components and ssr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,37 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class CustomDocument extends Document {
-  static async getInitialProps (ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+  static async getInitialProps(ctx:any) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App:any) => (props:any) => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
     return (
       <Html>
         <Head>
-        <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,400;0,500;1,400;1,500&family=Roboto+Mono:ital,wght@0,300;0,400;0,500;1,500&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,400;0,500;1,400;1,500&family=Roboto+Mono:ital,wght@0,300;0,400;0,500;1,500&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
