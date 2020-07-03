6c623f8af6ef14710d41d0faec7e8622134b8648
iamseki <chrisyuri_19@hotmail.com>
2020-07-03 12:50:18 -0300

    fix: styles folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,38 @@
-import { Container, Form } from "./styles";
+import React , { useRef, useEffect } from 'react'
+import { Container, Form } from "../styles/index";
 import { GiRead } from 'react-icons/gi'
 import { FaMoneyCheckAlt } from 'react-icons/fa'
-const Home = () => (
-  <Container>
-    <Form>
-      <GiRead size="40"/>
-      <input
-          placeholder="O que você quer saber ?"
+
+const Home = () => {
+
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080/room")
+    ws.onopen = () => alert("socket conectado")
+
+
+    },[])
+
+  return (
+    <Container>
+      <Form>
+        <GiRead size="40"/>
+        <input
+            placeholder="O que você quer saber ?"
+        />
+        <FaMoneyCheckAlt size="32"/>
+      </Form>
+      <div style={{display:'flex', alignItems:'center',justifyContent:'center', marginTop:50}}>
+      <img 
+        src="/codingws.svg"
+        alt="codingws"
+        height="400"
+        width="400"
       />
-      <FaMoneyCheckAlt size="32"/>
-    </Form>
-    <div style={{display:'flex', alignItems:'center',justifyContent:'center', marginTop:50}}>
-    <img 
-      src="/codingws.svg"
-      alt="codingws"
-      height="400"
-      width="400"
-    />
-    </div>
-  </Container>
-)
+      </div>
+    </Container>
+  )
+}
+
 
 export default Home;
