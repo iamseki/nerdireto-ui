a41cec54bf8cc873070be2579bee28469bd329ca
iamseki <chrisyuri_19@hotmail.com>
2020-07-05 00:05:42 -0300

    adjust load document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,39 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react'
 import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
-class CustomDocument extends Document {
-  static async getInitialProps(ctx:any) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App:any) => (props:any) => sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
-    }
-  }
-
+class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-br">
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,400;0,500;1,400;1,500&family=Roboto+Mono:ital,wght@0,300;0,400;0,500;1,500&display=swap" rel="stylesheet"/>
-          <meta name="og:description" content="Sendo direto e reto sem enrolamento, venha disseminar o conhecimento !"/>
-          <meta name="description" content="Sendo direto e reto sem enrolamento, venha disseminar o conhecimento !"/>
+          <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,400;0,500;1,400;1,500&family=Roboto+Mono:ital,wght@0,300;0,400;0,500;1,500&display=swap" rel="stylesheet" />
+          <meta name="og:description" content="Sendo direto e reto sem enrolamento, venha disseminar o conhecimento !" />
+          <meta name="description" content="Sendo direto e reto sem enrolamento, venha disseminar o conhecimento !" />
         </Head>
         <body>
           <Main />
@@ -44,4 +21,28 @@ class CustomDocument extends Document {
   }
 }
 
-export default CustomDocument;
+MyDocument.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet()
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+    })
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement(), sheet.getStyleElement()],
+  };
+};
+
+export default MyDocument;
