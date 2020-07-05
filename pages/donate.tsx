90ad8c71fec3463350d25ed08e58b708ffd84037
iamseki <chrisyuri_19@hotmail.com>
2020-07-05 02:34:53 -0300

    adjust donate page

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -1,10 +1,13 @@
-const Donate = () => (<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
+const Donate = () => (<div style={{ display: 'flex',flexDirection:'column', alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
   <img
     src="/investing.svg"
     alt="codingws"
     height="500"
     width="500"
   />
+  <h2>Interessado em fazer uma doação ?</h2>
+  <h3 style={{margin:10, letterSpacing:2}}>nerdireto@gmail.com</h3>
+  <strong></strong>
 </div>)
 
 export default Donate
