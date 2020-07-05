8c7cffca7792d2cddac7f6402824dce9b9c4f446
iamseki <chrisyuri_19@hotmail.com>
2020-07-05 02:20:16 -0300

    add donate and linking read and cash icons

---
+++ pages/donate.tsx
@@ -0,0 +1,10 @@
+const Donate = () => (<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
+  <img
+    src="/investing.svg"
+    alt="codingws"
+    height="500"
+    width="500"
+  />
+</div>)
+
+export default Donate
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import React , {  useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Container, Form } from "../styles/index";
 import { GiRead } from 'react-icons/gi'
 import { FaMoneyCheckAlt } from 'react-icons/fa'
 import Tooltip from '@material-ui/core/Tooltip';
+import Link from 'next/link'
 
 const Home = () => {
 
@@ -10,28 +11,32 @@ const Home = () => {
     const ws = new WebSocket("ws://localhost:8080/room")
     ws.onopen = () => alert("socket conectado")
 
-    },[])
+  }, [])
 
   return (
     <Container>
       <Form>
-        <Tooltip title="Escrever" arrow>
-          <IconRead />
-        </Tooltip>
+        <Link href="/blog">
+          <Tooltip title="Escrever" arrow>
+            <IconRead />
+          </Tooltip>
+        </Link>
         <input
-            placeholder="O que você quer saber ?"
+          placeholder="O que você quer saber ?"
         />
-        <Tooltip title="Doar uns trocados" arrow>
-          <IconMoney />
-        </Tooltip>
+        <Link href="/donate">
+          <Tooltip title="Doar uns trocados" arrow>
+            <IconMoney />
+          </Tooltip>
+        </Link>
       </Form>
-      <div style={{display:'flex', alignItems:'center',justifyContent:'center', marginTop:50}}>
-      <img 
-        src="/codingws.svg"
-        alt="codingws"
-        height="400"
-        width="400"
-      />
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
+        <img
+          src="/codingws.svg"
+          alt="codingws"
+          height="400"
+          width="400"
+        />
       </div>
     </Container>
   )
@@ -40,13 +45,13 @@ const Home = () => {
 const IconMoney = React.forwardRef(function IconMoney(props, ref) {
   return (<div {...props} ref={ref as React.MutableRefObject<HTMLDivElement>}>
     <FaMoneyCheckAlt size="32" />
-    </div>)
+  </div>)
 });
 
 const IconRead = React.forwardRef(function IconRead(props, ref) {
   return (<div {...props} ref={ref as React.MutableRefObject<HTMLDivElement>}>
     <GiRead size="40" />
-    </div>)
+  </div>)
 });
 
 
