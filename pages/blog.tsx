a6f0b07ac79b2a65e38303271ebfe6ec8d28e1bf
iamseki <chrisyuri_19@hotmail.com>
2020-07-06 01:55:11 -0300

    change blog page to be rendering every time

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 import Card from '../components/Card'
 import { Container } from '../styles/blog'
 
@@ -22,16 +22,12 @@ const Blog = ({ posts }: Props) => {
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
 
   const res = await fetch('https://notion-api.splitbee.io/v1/table/d5fe857e5cb04182b0c78c302bb4fd38?v=f148e891fc3549d0ad8ad5b88109a39b')
   const posts = await res.json()
 
-  return {
-    props: {
-      posts,
-    },
-  }
+  return { props: { posts } }
 }
 
 export default Blog
