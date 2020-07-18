8e014872ca99cc9bf576805b351410822ebff124
iamseki <chrisyuri_19@hotmail.com>
2020-07-18 12:04:11 -0300

    storage dark mode state in localstorage

--- components/Header/Switch/index.tsx
+++ components/Header/Switch/index.tsx
@@ -1,19 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Switch from '@material-ui/core/Switch';
 
 interface Props {
   SwitchThemeHandler: Function;
 }
 
-const  Switches = ({SwitchThemeHandler}: Props) => {
+const Switches = ({ SwitchThemeHandler }: Props) => {
   const [checked, setCheked] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCheked(!checked);
+    localStorage.setItem("Nerdi@Dark", String(!checked))
   };
 
-  useEffect(()=>
-    checked ? SwitchThemeHandler('dark'):SwitchThemeHandler('light'),[checked])
+  useEffect(() => {
+    const check = localStorage.getItem("Nerdi@Dark") == 'true' ? true : false
+
+    if (check === true) setCheked(true)
+
+  }, [])
+
+  useEffect(() =>
+    checked ? SwitchThemeHandler('dark') : SwitchThemeHandler('light'), [checked])
 
   return (
     <div>
