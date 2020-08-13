c3942a975c4dba06939dd7898ea01c53d0550330
iamseki <chrisyuri_19@hotmail.com>
2020-08-13 13:22:45 -0300

    Turn blog page responsive

--- styles/blog.ts
+++ styles/blog.ts
@@ -1,10 +1,20 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  column-gap: 10px;
-  row-gap: 1em;
+  @media (min-width: 700px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 10px;
+    row-gap: 1em;
+}
+
+@media (max-width: 699px) {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
 `
 export const LoadingBox = styled.div`
   display: flex;
